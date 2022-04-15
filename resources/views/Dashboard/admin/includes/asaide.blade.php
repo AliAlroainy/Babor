b614a770d187eee62b12166f360f5c12fcf1dd8f
Ali <aliabduh.2020@gmail.com>
2022-04-15 15:23:59 +0300

    add the Admin Dashboard view and assets files

---
+++ resources/views/Dashboard/admin/includes/asaide.blade.php
@@ -0,0 +1,166 @@
+
+<!-- Menu -->
+
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+
+    
+  
+    <div class="app-brand demo ">
+  
+      <a href="index-2.html" class="app-brand-link">
+        <span class="app-brand-logo demo">
+  
+  
+  
+  </span>
+        <span class="app-brand-text demo menu-text fw-bolder ms-2">WorkTime</span>
+      </a>
+  
+      <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+        <i class="bx bx-chevron-left bx-sm align-middle"></i>
+      </a>
+    </div>
+  
+    <div class="menu-inner-shadow"></div>
+  
+    
+    <ul class="menu-inner py-1">
+      <!-- Dashboards -->
+      <li class="menu-item">
+        <a href="javascript:void(0);" class="menu-link ">
+          <i class="menu-icon tf-icons bx bx-home-circle"></i>
+          <div data-i18n="Dashboards">Dashboards</div>
+        </a>
+     
+      </li>
+  
+      <!-- Layouts -->
+      <li class="menu-item">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class="menu-icon tf-icons bx bx-layout"></i>
+          <div data-i18n="Companies">Companies</div>
+        </a>
+  
+        <ul class="menu-sub">
+  
+          <li class="menu-item">
+            <a href="layouts-collapsed-menu.html" class="menu-link">
+              <div data-i18n="Add">Add</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="layouts-content-navbar.html" class="menu-link">
+              <div data-i18n="View">View</div>
+            </a>
+          </li>
+
+        </ul>
+      </li>
+  
+      <!-- Apps & Pages -->
+      <li class="menu-header small text-uppercase">
+        <span class="menu-header-text">Apps &amp; Pages</span>
+      </li>
+      <li class="menu-item">
+        <a href="app-calendar.html" class="menu-link">
+          <i class="menu-icon tf-icons bx bx-calendar"></i>
+          <div data-i18n="Calendar">Calendar</div>
+        </a>
+      </li>
+      <li class="menu-item">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class='menu-icon tf-icons bx bx-food-menu'></i>
+          <div data-i18n="Invoice">Invoice</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item">
+            <a href="app-invoice-list.html" class="menu-link">
+              <div data-i18n="List">List</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="app-invoice-preview.html" class="menu-link">
+              <div data-i18n="Preview">Preview</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="app-invoice-edit.html" class="menu-link">
+              <div data-i18n="Edit">Edit</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="app-invoice-add.html" class="menu-link">
+              <div data-i18n="Add">Add</div>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="menu-item active open">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class="menu-icon tf-icons bx bx-user"></i>
+          <div data-i18n="Users">Users</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item">
+            <a href="users" class="menu-link">
+              <div data-i18n="List">List</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="users" class="menu-link">
+              <div data-i18n="Add">Add</div>
+            </a>
+          </li>
+         
+        </ul>
+      </li>
+      <li class="menu-item">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class='menu-icon tf-icons bx bx-check-shield'></i>
+          <div data-i18n="Jobs">Jobs</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item">
+            <a href="app-access-roles.html" class="menu-link">
+              <div data-i18n="Add">Add</div>
+            </a>
+          </li>
+          <li class="menu-item">
+            <a href="app-access-permission.html" class="menu-link">
+              <div data-i18n="View">View</div>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="menu-item">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+          <i class="menu-icon tf-icons bx bx-dock-top"></i>
+          <div data-i18n="Ads">Ads</div>
+        </a>
+        <ul class="menu-sub">
+          <li class="menu-item">
+            <a href="javascript:void(0);" class="menu-link">
+              <div data-i18n="Add">Add</div>
+            </a>
+            <li class="menu-item">
+              <a href="pages-profile-user.html" class="menu-link">
+                <div data-i18n="View">View</div>
+              </a>
+            </li>
+           
+              
+              
+            </ul>
+          </li>
+      
+     
+     
+ 
+      
+      
+ 
+  
+
+  
+  </aside>
+  <!-- / Menu -->
