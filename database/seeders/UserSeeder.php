54297e3e2b4cbef21e45d4b70c7f1d451356f333
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-21 16:40:02 +0300

    fix: import required classes

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
