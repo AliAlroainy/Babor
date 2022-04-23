98753bb1326afa3eeefff9dfc624787e7fe8058c
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-23 17:41:35 +0300

    add style for login modal

--- resources/views/Front/include/modals.blade.php
+++ resources/views/Front/include/modals.blade.php
@@ -1,22 +1,32 @@
 ]<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" dir="rtl">
+<form class="modal fade needs-validation" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" dir="rtl"novalidate>
   <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
+    <div class="modal-content d-flex align-items-center ">
+      <div class="modal-header align-items-center ">
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 
-        <h3 class="modal-title text-center" id="exampleModalLabel">تسجيل الدخول</h3>
+        <h3 class=" modal-title " id="exampleModalLabel">تسجيل الدخول</h3>
       </div>
-      <div class="modal-body">
+      <div class="modal-body d-flex align-items-center justify-content-center flex-column ">
+      <!-- modal boday -->
+
+        <div class="col-md-8 position-relative">
+          <label for="validationTooltip01" class="form-label">البريد الالكتروني</label>
+          <input type="text" class="form-control" id="validationTooltip01" value="Mark" required>
+          <div class="valid-tooltip">
+            Looks good!
+          </div>
+        </div>
+
+<input class="form-control" type="text" placeholder="Default input" aria-label="default input example">
 
 
-
-
-       
+        <button type="submit" class="btn btn-dark  justify-content-end">تسجيل الدخول</button>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-dark">ليس لديك حساب</button>
+       
+        <button type="button" class="btn btn-light">ليس لديك حساب</button>
       </div>
     </div>
   </div>
-</div>
+</form>
