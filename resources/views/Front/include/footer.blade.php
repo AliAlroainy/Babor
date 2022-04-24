219a26bad70b0a8a696edb5715d57e8b6d1f5d32
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-24 12:57:57 +0300

    add regester model

--- resources/views/Front/include/footer.blade.php
+++ resources/views/Front/include/footer.blade.php
@@ -162,10 +162,10 @@
 
 
   
-	<script src="js/slick.min.js"></script>
+	{{-- <script src="js/slick.min.js"></script>
     <script src="js/nouislider.min.js"></script>
     <script src="js/jquery.zoom.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main.js"></script> --}}
 	
 	
 
--- resources/views/Front/include/modals.blade.php
+++ resources/views/Front/include/modals.blade.php
@@ -1,8 +1,8 @@
-<!-- Modal -->
-<form class="modal fade needs-validation" id="modal_login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" dir="rtl"novalidate>
+<!-- Start Login Modal -->
+<form class="modal fade " id="modal_login" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" dir="rtl">
   <div class="modal-dialog">
-    <div class="modal-content d-flex align-items-center ">
-      <div class="modal-header  w-100 align-items-center ">
+    <div class="modal-content ">
+      <div class="modal-header   ">
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 
         <h3 class=" w-100 text-center " id="exampleModalLabel">تسجيل الدخول</h3>
@@ -10,11 +10,12 @@
       <div class="modal-body w-100 p-5  ">
       <!-- modal boday -->
         <div > <img src="svg\login.svg" class="" /> </div>
-        <div class=" align-items-center"> 
+
+        <div class=" align-items-center mt-2"> 
       <div class="form-group">
         <div class="input-group">
             <span class="input-group-addon">
-                <i class="fa fa-envelope"></i>
+                {{-- <i class="fa fa-envelope"></i> --}}
             </span>
             <input type="email"  name="email" class="form-control form-control-lg" placeholder=" البريد الالكتروني" />
         </div>
@@ -23,14 +24,14 @@
     <div class="form-group">
         <div class="input-group">
             <span class="input-group-addon">
-                <i class="fa fa-unlock-alt" ></i>
+                {{-- <i class="fa fa-unlock-alt" ></i> --}}
             </span>
             <input type="password"  name="password" class="form-control form-control-lg" placeholder="كلمة المرور" />
 
         </div>
         
     </div>
-    <small class="form-text link "> نسيت كلمة المرور؟</small>
+    <small class="form-text link "> هل نسيت كلمة المرور؟</small>
 <br/>
     <label class=" custom-control custom-checkbox">
         <input type="checkbox" id="modal_login_remember" class="custom-control-input" />
@@ -40,15 +41,76 @@
 </div>
        
 </div>
-      <div class="modal-footer">
-        <button type="submit" class="btn btn-dark  justify-content-end">تسجيل الدخول</button>
-        
+      <div class="modal-footer d-flex align-items-center justify-content-center" >
+      
+        <button type="submit" class="btn btn-dark  ">تسجيل الدخول</button>
+      
         
       </div>
-      <button type="button" class="btn btn-light">ليس لديك حساب</button>
+      <button type="button" data-toggle="modal" data-target="#modal_reg" class="btn btn-light">ليس لديك حساب</button>
     </div>
   
   </div>
 </form>
+
+<!-- End Login Modal -->
+
+
+<!-- Regeister  Modal -->
+<form class="modal fade " id="modal_reg" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" dir="rtl">
+  <div class="modal-dialog">
+    <div class="modal-content ">
+      <div class="modal-header   ">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+
+        <h3 class=" w-100 text-center " id="exampleModalLabel">تسجيل بالموقع</h3>
+      </div>
+      <div class="modal-body w-100 p-5  ">
+      <!-- modal boday -->
+        <div > <img src="svg\login.svg" class="" /> </div>
+
+        <div class=" align-items-center mt-2"> 
+      <div class="form-group">
+        <div class="input-group">
+            <span class="input-group-addon">
+                {{-- <i class="fa fa-envelope"></i> --}}
+            </span>
+            <input type="email"  name="email" class="form-control form-control-lg" placeholder=" البريد الالكتروني" />
+        </div>
+        <small class="form-text "> لن نقوم بمشاركة بياناتك مع طرف اخر</small>
+    </div>
+    <div class="form-group">
+        <div class="input-group">
+            <span class="input-group-addon">
+                {{-- <i class="fa fa-unlock-alt" ></i> --}}
+            </span>
+            <input type="password"  name="password" class="form-control form-control-lg" placeholder="كلمة المرور" />
+
+        </div>
+        
+    </div>
+<br/>
+    <label class=" custom-control custom-checkbox">
+        <input type="checkbox" id="modal_login_remember" class="custom-control-input" />
+        <span class="custom-control-indicator"></span>
+        <span class="custom-control-description"> اوافق على شروط الخصوصية </span>
+    </label>
+</div>
+       
+</div>
+      <div class="modal-footer d-flex align-items-center justify-content-center" >
+      
+        <button type="submit" class="btn btn-dark  ">تسجيل </button>
+      
+        
+      </div>
+      <button type="button" data-toggle="modal" data-target="#modal_log" class="btn btn-light"> لديك حساب</button>
+    </div>
+  
+  </div>
+</form>
+
+<!-- End Regeister Modal -->
+
 
 
