b20f63c6807759813af43bde4410f33ee162cf1a
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-24 15:35:29 +0300

    fix : fix layout to message

--- resources/views/Front/contact.blade.php
+++ resources/views/Front/contact.blade.php
@@ -24,53 +24,53 @@
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>اسمك<span>*</span></label>
-												<input name="name" type="text" placeholder=" علي عبده">
+												<input name="name" type="text" placeholder=" علي عبده" required>
 											</div>
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>عنوان رسالتك<span>*</span></label>
-												<input name="subject" type="text" placeholder="">
+												<input name="subject" type="text" placeholder="" required>
 											</div>
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>عنوان ايميلك<span>*</span></label>
-												<input name="email" type="email" placeholder=" ">
+												<input name="email" type="email" placeholder=" " required>
 											</div>	
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>رقم تلفونك<span>*</span></label>
-												<input name="company_name" type="text" placeholder=" 777 777 777">
+												<input name="company_name" type="text" placeholder=" 777 777 777" required>
 											</div>	
 										</div>
 										<div class="col-12">
 											<div class="form-group message">
 												<label> محتوى رسالتك<span>*</span></label>
-												<textarea name="message" placeholder="اكتب هنا ..."></textarea>
+												<textarea name="message" placeholder="اكتب هنا ..." required></textarea>
 											</div>
 										</div>
 										<div class="col-12">
 											<div class="form-group button">
-												<button type="submit" class="btn ">ارسال الرسالة </button>
+												<button type="submit" class="btn btn-light ">ارسال الرسالة </button>
 											</div>
 										</div>
 									</div>
 								</form>
 							</div>
 						</div>
-						<div class="col-lg-4 col-12"  dir="ltr">
+						<div class="col-lg-4 col-12 "  >
 							<div class="single-head" >
-								<div class="single-info "  >
+								<div class="single-info  text-end"  >
 									<i class="fa fa-phone"></i>
 									<h4 class="title">اتصل بنا:</h4>
-									<ul>
+									<ul >
 										<li>+123 456-789-1120</li>
 										<li>+522 672-452-1120</li>
 									</ul>
 								</div>
-								<div class="single-info">
+								<div class="single-info text-end">
 									<i class="fa fa-envelope-open"></i>
 									<h4 class="title">عنوان بريدنا:</h4>
 									<ul>
@@ -78,9 +78,9 @@
 										<li><a href="mailto:info@babor.com">support@babor.com</a></li>
 									</ul>
 								</div>
-								<div class="single-info">
+								<div class="single-info text-end">
 									<i class="fa fa-location-arrow"></i>
-									<h4 class="title">موقعنا على الخريطة:</h4>
+									<h4 class="title">موقعنا:</h4>
 									<ul>
 										<li>المسبح , تعز - اليمن </li>
 									</ul>
