5fe58cc4861b7839c75cb82296fb2ebe36a3b6f3
A-Abd <abrar.abdulwahed@gmail.com>
2022-04-25 20:52:12 +0300

    fix: avatar of account in admin

--- resources/views/admin/accounts/users.blade.php
+++ resources/views/admin/accounts/users.blade.php
@@ -36,7 +36,13 @@
                                             @if (!$loop->first)
                                                 <tr>
                                                     <td class="py-1">
-                                                        <img src="/images/profiles/{{ $user->avatar }}" alt="image" />
+                                                        @if (isset($user->profile->avatar))
+                                                            <img src="/images/profiles/{{ $user->profile->avatar }}"
+                                                                alt="profile" class="img-lg rounded-circle mb-3" />
+                                                        @else
+                                                            <img src="/images/profiles/default.jpg" alt="profile"
+                                                                class="img-lg rounded-circle mb-3" />
+                                                        @endif
                                                     </td>
                                                     <td>
                                                         {{ $user->name }}
