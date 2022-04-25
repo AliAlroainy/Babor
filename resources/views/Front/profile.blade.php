e1a45826e778376a6a843f63841d9875b6835df2
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-25 23:53:24 +0300

    feat: add time line to profile

--- resources/views/Front/profile.blade.php
+++ resources/views/Front/profile.blade.php
@@ -38,17 +38,17 @@
               <h4> حمد بكيل</h4>
               <ul class="list-inline mb-0 d-flex align-items-center flex-wrap justify-content-sm-start justify-content-center gap-2">
                 <li class="list-inline-item fw-semibold">
-                  <i class='bx bx-pen'></i> مدير مبيعات
+                  <i class='bx bx-pen'></i> مدير مبيعات -
                 </li>
                 <li class="list-inline-item fw-semibold">
-                  <i class='bx bx-map'></i> مدينة تعز
+                  <i class='bx bx-map'></i> مدينة تعز -
                 </li>
                 <li class="list-inline-item fw-semibold">
                   <i class='bx bx-calendar-alt'></i> انضم بتاريخ 2022
                 </li>
               </ul>
             </div>
-            <a href="#" class="btn btn-dark text-nowrap">
+            <a href="#" class="btn btn-warning text-nowrap">
               <i class='bx bx-user-check'></i> تواصل 
             </a>
           </div>
@@ -63,10 +63,10 @@
 <div class="row">
   <div class="col-md-12">
     <ul class="nav nav-pills flex-column flex-sm-row mb-4">
-      <li class="nav-item"><a class="nav-link active" href="#"><i class='bx bx-user'></i> البروفايل</a></li>
-      <li class="nav-item"><a class="nav-link" href="#"><i class='bx bx-group'></i> الفريق</a></li>
-      <li class="nav-item"><a class="nav-link" href="#"><i class='bx bx-grid-alt'></i> المبيعات</a></li>
-      <li class="nav-item"><a class="nav-link" href="#"><i class='bx bx-link-alt'></i> المشتريات</a></li>
+      <li class="nav-item"><a class="nav-link active bg-dark" href="#"><i class='bx bx-user'></i> البروفايل</a></li>
+      <li class="nav-item"><a class="nav-link link-dark" href="#"><i class='bx bx-group'></i> الفريق</a></li>
+      <li class="nav-item"><a class="nav-link link-dark" href="#"><i class='bx bx-grid-alt'></i> المبيعات</a></li>
+      <li class="nav-item"><a class="nav-link link-dark" href="#"><i class='bx bx-link-alt'></i> المشتريات</a></li>
     </ul>
   </div>
 </div>
@@ -112,19 +112,7 @@
     <div class="card card-action mb-4">
       <div class="card-header align-items-center">
         <h5 class="card-action-title mb-0"><i class='bx bx-list-ul me-2'></i>ابرز الانشطة </h5>
-        <div class="card-action-element">
-          <div class="dropdown">
-            <button type="button" class="btn dropdown-toggle hide-arrow p-0" data-bs-toggle="dropdown" aria-expanded="false"><i class="bx bx-dots-vertical-rounded"></i></button>
-            <ul class="dropdown-menu dropdown-menu-end">
-              <li><a class="dropdown-item" href="javascript:void(0);">مشاركة </a></li>
-              <li><a class="dropdown-item" href="javascript:void(0);">اقتراح</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="javascript:void(0);">ابلاغ عن خطاء</a></li>
-            </ul>
-          </div>
-        </div>
+ 
       </div>
       <div class="card-body">
         <ul class="timeline ms-2">
@@ -136,64 +124,35 @@
                 <small class="text-muted">اليوم</small>
               </div>
               <p class="mb-2">عقد صفقة بيع الساعة 12:40PM </p>
-              <div class="d-flex flex-wrap">
-                <div class="avatar me-3">
-                  <img src="assets/img/avatars/3.png" alt="Avatar" class="rounded-circle" />
+             
+              </div>
+
+              <div class="timeline-event">
+                <div class="timeline-header mb-1">
+                  <h6 class="mb-0"> الدخول بمزاد</h6>
+                  <small class="text-muted">اليوم</small>
                 </div>
-                <div>
-                  <h6 class="mb-0">Lester McCarthy (Client)</h6>
-                  <span>CEO of Infibeam</span>
+                <p class="mb-2">عقد صفقة بيع الساعة 12:40PM </p>
+               
                 </div>
-              </div>
+
+                <div class="timeline-event">
+                    <div class="timeline-header mb-1">
+                      <h6 class="mb-0"> الدخول بمزاد</h6>
+                      <small class="text-muted">اليوم</small>
+                    </div>
+                    <p class="mb-2">عقد صفقة بيع الساعة 12:40PM </p>
+                   
+                    </div>
             </div>
           </li>
-          <li class="timeline-item timeline-item-transparent">
-            <span class="timeline-point timeline-point-info"></span>
-            <div class="timeline-event">
-              <div class="timeline-header mb-1">
-                <h6 class="mb-0">Create a new project for client</h6>
-                <small class="text-muted"> قبل 2 يوم</small>
-              </div>
-              <p class="mb-0">Add files to new design folder</p>
-            </div>
-          </li>
-          <li class="timeline-item timeline-item-transparent">
-            <span class="timeline-point timeline-point-primary"></span>
-            <div class="timeline-event">
-              <div class="timeline-header mb-1">
-                <h6 class="mb-0">Shared 2 New Project Files</h6>
-                <small class="text-muted">6 Day Ago</small>
-              </div>
-              <p class="mb-2">Sent by Mollie Dixon <img src="assets/img/avatars/4.png" class="rounded-circle ms-3" alt="avatar" height="20" width="20"></p>
-              <div class="d-flex flex-wrap gap-2">
-                <a href="javascript:void(0)" class="me-3">
-                  <img src="assets/img/icons/misc/pdf.png" alt="Document image" width="20" class="me-2">
-                  <span class="h6">App Guidelines</span>
-                </a>
-                <a href="javascript:void(0)">
-                  <img src="assets/img/icons/misc/doc.png" alt="Excel image" width="20" class="me-2">
-                  <span class="h6">Testing Results</span>
-                </a>
-              </div>
-            </div>
-          </li>
-          <li class="timeline-item timeline-item-transparent">
-            <span class="timeline-point timeline-point-success"></span>
-            <div class="timeline-event pb-0">
-              <div class="timeline-header mb-1">
-                <h6 class="mb-0">Project status updated</h6>
-                <small class="text-muted">10 Day Ago</small>
-              </div>
-              <p class="mb-0">Woocommerce iOS App Completed</p>
-            </div>
-          </li>
-          <li class="timeline-end-indicator">
-            <i class="bx bx-check-circle"></i>
-          </li>
+          
+
         </ul>
       </div>
     </div>
     <!--/ Activity Timeline -->
+
 
 
   </div>
