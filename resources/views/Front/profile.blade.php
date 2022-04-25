b8f0c702c0a0267d886681cd834f846850d80ac8
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-25 23:09:40 +0300

    add arabic style

--- resources/views/Front/profile.blade.php
+++ resources/views/Front/profile.blade.php
@@ -25,7 +25,7 @@
 <div class="row">
   <div class="col-12">
     <div class="card mb-4">
-      <div class="user-profile-header-banner">
+      <div class="user-profile-header-banner ">
         <img src="assets/img/pages/profile-banner.png" alt="Banner image" class="rounded-top">
       </div>
       <div class="user-profile-header d-flex flex-column flex-sm-row text-sm-start text-center mb-4">
@@ -34,22 +34,22 @@
         </div>
         <div class="flex-grow-1 mt-3 mt-sm-5">
           <div class="d-flex align-items-md-end align-items-sm-start align-items-center justify-content-md-between justify-content-start mx-4 flex-md-row flex-column gap-4">
-            <div class="user-profile-info">
-              <h4>John Doe</h4>
+            <div class="user-profile-info text-end">
+              <h4> حمد بكيل</h4>
               <ul class="list-inline mb-0 d-flex align-items-center flex-wrap justify-content-sm-start justify-content-center gap-2">
                 <li class="list-inline-item fw-semibold">
-                  <i class='bx bx-pen'></i> UX Designer
+                  <i class='bx bx-pen'></i> مدير مبيعات
                 </li>
                 <li class="list-inline-item fw-semibold">
-                  <i class='bx bx-map'></i> Vatican City
+                  <i class='bx bx-map'></i> مدينة تعز
                 </li>
                 <li class="list-inline-item fw-semibold">
-                  <i class='bx bx-calendar-alt'></i> Joined April 2021
+                  <i class='bx bx-calendar-alt'></i> انضم بتاريخ 2022
                 </li>
               </ul>
             </div>
             <a href="javascript:void(0)" class="btn btn-primary text-nowrap">
-              <i class='bx bx-user-check'></i> Connected
+              <i class='bx bx-user-check'></i> تواصل 
             </a>
           </div>
         </div>
@@ -63,10 +63,10 @@
 <div class="row">
   <div class="col-md-12">
     <ul class="nav nav-pills flex-column flex-sm-row mb-4">
-      <li class="nav-item"><a class="nav-link active" href="javascript:void(0);"><i class='bx bx-user'></i> Profile</a></li>
-      <li class="nav-item"><a class="nav-link" href="pages-profile-teams.html"><i class='bx bx-group'></i> Teams</a></li>
-      <li class="nav-item"><a class="nav-link" href="pages-profile-projects.html"><i class='bx bx-grid-alt'></i> Projects</a></li>
-      <li class="nav-item"><a class="nav-link" href="pages-profile-connections.html"><i class='bx bx-link-alt'></i> Connections</a></li>
+      <li class="nav-item"><a class="nav-link active" href="javascript:void(0);"><i class='bx bx-user'></i> البروفايل</a></li>
+      <li class="nav-item"><a class="nav-link" href="pages-profile-teams.html"><i class='bx bx-group'></i> الفريق</a></li>
+      <li class="nav-item"><a class="nav-link" href="pages-profile-projects.html"><i class='bx bx-grid-alt'></i> المبيعات</a></li>
+      <li class="nav-item"><a class="nav-link" href="pages-profile-connections.html"><i class='bx bx-link-alt'></i> المشتريات</a></li>
     </ul>
   </div>
 </div>
@@ -78,19 +78,18 @@
     <!-- About User -->
     <div class="card mb-4">
       <div class="card-body">
-        <small class="text-muted text-uppercase">About</small>
+        <small class="text-muted text-uppercase">نبذة</small>
         <ul class="list-unstyled mb-4 mt-3">
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-user"></i><span class="fw-semibold mx-2">Full Name:</span> <span>John Doe</span></li>
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-check"></i><span class="fw-semibold mx-2">Status:</span> <span>Active</span></li>
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-star"></i><span class="fw-semibold mx-2">Role:</span> <span>Developer</span></li>
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-flag"></i><span class="fw-semibold mx-2">Country:</span> <span>USA</span></li>
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-detail"></i><span class="fw-semibold mx-2">Languages:</span> <span>English</span></li>
+          <li class="d-flex align-items-center mb-3"><i class="bx bx-user"></i><span class="fw-semibold mx-2">الاسم:</span> <span>حمد بكيل</span></li>
+          <li class="d-flex align-items-center mb-3"><i class="bx bx-check"></i><span class="fw-semibold mx-2">الحالة:</span> <span>نشط</span></li>
+          <li class="d-flex align-items-center mb-3"><i class="bx bx-star"></i><span class="fw-semibold mx-2">المهنة:</span> <span>مطور برمجيات</span></li>
+          <li class="d-flex align-items-center mb-3"><i class="bx bx-flag"></i><span class="fw-semibold mx-2">الدولة:</span> <span>اليمن</span></li>
         </ul>
-        <small class="text-muted text-uppercase">Contacts</small>
+        <small class="text-muted text-uppercase">معلومات التواصل</small>
         <ul class="list-unstyled mb-4 mt-3">
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-phone"></i><span class="fw-semibold mx-2">Contact:</span> <span>(123) 456-7890</span></li>
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-chat"></i><span class="fw-semibold mx-2">Skype:</span> <span>john.doe</span></li>
-          <li class="d-flex align-items-center mb-3"><i class="bx bx-envelope"></i><span class="fw-semibold mx-2">Email:</span> <span>john.doe@example.com</span></li>
+          <li class="d-flex align-items-center mb-3"><i class="bx bx-phone"></i><span class="fw-semibold mx-2">الهاتف:</span> <span>(123) 456-7890</span></li>
+          <li class="d-flex align-items-center mb-3"><i class="bx bx-chat"></i><span class="fw-semibold mx-2">فيسبوك:</span> <span>john.doe</span></li>
+          <li class="d-flex align-items-center mb-3"><i class="bx bx-envelope"></i><span class="fw-semibold mx-2">ايميل:</span> <span>john.doe@example.com</span></li>
         </ul>
         <small class="text-muted text-uppercase">Teams</small>
         <ul class="list-unstyled mt-3 mb-0">
