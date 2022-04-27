fbdc7ab8087cf0eefdc7c671e3963af9a57c86dd
ReemAlshathly <reem250139@gmail.com>
2022-04-27 19:42:36 +0300

    create bids table

--- database/migrations/2022_04_27_162344_create_bids_table.php
+++ database/migrations/2022_04_27_162344_create_bids_table.php
@@ -16,6 +16,17 @@ return new class extends Migration
         Schema::create('bids', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->integre('currentPrice');
+        
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->constrained()
+                  ->references('id')->on('users')
+                  ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->unsignedBigInteger('auction_id');
+            $table->foreign('auction_id')->constrained()
+                  ->references('id')->on('auctions')
+                  ->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
--- database/migrations/2022_04_27_163209_create_cars_table.php
+++ database/migrations/2022_04_27_163209_create_cars_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            
             $table->timestamps();
         });
     }
