1ccd06c307313cc12816047b7e61c69186bc1730
ReemAlshathly <reem250139@gmail.com>
2022-04-27 19:56:28 +0300

    create categories table

--- database/migrations/2022_04_27_163331_create_categories_table.php
+++ database/migrations/2022_04_27_163331_create_categories_table.php
@@ -16,6 +16,10 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('Image');
+            $table->boolean('status')->default(1);
+
         });
     }
 
