f4ace36cb1a802fb1da0ef67fb2a0308f3bfeacd
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-29 11:46:12 +0300

    fix : notfiction

--- resources/views/Front/include/header.blade.php
+++ resources/views/Front/include/header.blade.php
@@ -190,24 +190,18 @@
 									<ul class="shopping-list">
 										<li>
 											<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
-											<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
-											<h4><a href="#">Woman Ring</a></h4>
-											<p class="quantity">1x - <span class="amount">$99.00</span></p>
+											<a class="cart-img" href="#"><img src="img/c1.jpg" alt="#"></a>
+											<h4><a href="#">تبقى يومين </a></h4>
+											<p class="quantity">مزاد سيارة لاندكروسر <span class="amount">$9990.00</span></p>
 										</li>
 										<li>
 											<a href="#" class="remove" title="Remove this item"><i class="fa fa-remove"></i></a>
-											<a class="cart-img" href="#"><img src="https://via.placeholder.com/70x70" alt="#"></a>
-											<h4><a href="#">Woman Necklace</a></h4>
-											<p class="quantity">1x - <span class="amount">$35.00</span></p>
+											<a class="cart-img" href="#"><img src="img/c1.jpg" alt="#"></a>
+											<h4><a href="#">  سيارة شفرليه </a></h4>
+											<p class="quantity"> عرض قد يهمك <span class="amount">$3905.00</span></p>
 										</li>
 									</ul>
-									<div class="bottom">
-										<div class="total">
-											<span>Total</span>
-											<span class="total-amount">$134.00</span>
-										</div>
-										<a href="checkout.html" class="btn animate">Checkout</a>
-									</div>
+								
 								</div>
 								<!--/ End notfications Item -->
                             </div>
