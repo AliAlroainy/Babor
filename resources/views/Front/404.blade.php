ac0165114886bf53cc13f00e906c1675ec90f74b
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-30 15:25:15 +0300

    add 404 svg

--- resources/views/Front/404.blade.php
+++ resources/views/Front/404.blade.php
@@ -1,9 +1,14 @@
 @include('Front.include.header')
 
 
-<div class="container d-flex align-items-center justify-content-center w-100 h-100" dir="rtl">
-     <h1> خطاء صفحة غير موجودة </h1>
-     <h4> يرجى التاكد من المسار الصحيح او العودة للصفحة الرئيسية </h4>
+<div class="container d-flex flex-column align-items-center justify-content-center w-100 h-100 mb-5" dir="rtl">
+    <img src="svg\404.svg" width="500" alt="404" />
+     <h1> خطاء 404    </h1>
+     <h2> صفحة غير موجودة</h2>
+    
+     <h4 class="text-muted"> يرجى التاكد من المسار الصحيح او العودة للصفحة الرئيسية </h4>
+
+     <a href="/" class="btn btn-warning">  الرئيسية</a>
 
 </div>
 
