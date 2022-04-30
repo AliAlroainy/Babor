2e1e94eb47630c94d76929bb6eba0f918cf36ffd
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-30 17:48:33 +0300

    feat :services components

--- resources/views/Front/services.blade.php
+++ resources/views/Front/services.blade.php
@@ -1,15 +1,96 @@
 @include('Front.include.header')
+<style>
+                      
+    </style>
+
+<div class="container d-flex flex-column align-items-center justify-content-center w-100 h-100" dir="rtl">
 
 
-<div class="container d-flex align-items-center justify-content-center w-100 h-100" dir="rtl">
 
-    <div class="row">
-        <div class="col-12">
-            <div class="section-title mt-4" >
-                <h2 >  خدماتنا </h2>
+
+<div class="container bootstrap snippets bootdey" >
+<section id="services" class="current">
+    <div class="services-top">
+        <div class="container bootstrap snippets bootdey">
+            <div class="row text-center">
+                <div class="col-sm-12 col-md-12 col-md-12">
+                    <h2 class="text-muted">  ماذا نقدم</h2>
+                    <h2 class="section-title" style="font-size: 60px;line-height: 60px;margin-bottom: 20px;font-weight: 900;">خدماتنا </h2>
+                    <p>نقدم خدمات <span class="highlight">بيع</span> و <span class="highlight">شراء</span> السيارات بشكل سلس وممتع وآمن.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-offset-1 col-sm-12 col-md-12 col-md-10">
+                    <div class="services-list">
+                        <div class="row">
+                            <div class="col-sm-6 col-md-4 col-md-4">
+                                <div class="service-block" style="visibility: visible;">
+                                    <div class="ico fa fa-magic highlight"></div>
+                                    <div class="text-block">
+                                        <div class="name">عروض حصرية </div>
+                                        <div class="info"> لمختلف انواع السيارات </div>
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-4 col-md-4">
+                                <div class="service-block" style="visibility: visible;">
+                                    <div class="ico fa fa-code highlight"></div>
+                                    <div class="text-block">
+                                        <div class="name">امان وتشفير</div>
+                                        <div class="info">Quality code that lasts</div>
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-4 col-md-4">
+                                <div class="service-block" style="visibility: visible;">
+                                    <div class="ico fa fa-pencil highlight"></div>
+                                    <div class="text-block">
+                                        <div class="name"> اليه للبيع والشراء</div>
+                                        <div class="info">Words that tell your story</div>
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-4 col-md-4">
+                                <div class="service-block" style="visibility: visible;">
+                                    <div class="ico fa fa-bullhorn highlight"></div>
+                                    <div class="text-block">
+                                        <div class="name">امكانية عرض سياراتك</div>
+                                        <div class="info">Converting users to customers</div>
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-4 col-md-4">
+                                <div class="service-block" style="visibility: visible;">
+                                    <div class="ico fa fa-eye highlight"></div>
+                                    <div class="text-block">
+                                        <div class="name">تجربة مستخدم ممتعه</div>
+                                        <div class="info">Leave a lasting impression</div>
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-4 col-md-4">
+                                <div class="service-block" style="visibility: visible;">
+                                    <div class="ico fa fa-umbrella highlight"></div>
+                                    <div class="text-block">
+                                        <div class="name">مزادات متنوعه</div>
+                                        <div class="info">Thinking beyond tomorrow</div>
+                                        <div class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+</section>
+</div>                    
 
 </div>
 
