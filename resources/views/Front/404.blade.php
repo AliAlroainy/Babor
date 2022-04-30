3cc90a0f4fac62ea35202d9acc2adad9341e7b94
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-04-30 15:15:40 +0300

    feat : add 404 page layouts

---
+++ resources/views/Front/404.blade.php
@@ -0,0 +1,11 @@
+@include('Front.include.header')
+
+
+<div class="container d-flex align-items-center justify-content-center w-100 h-100" dir="rtl">
+     <h1> خطاء صفحة غير موجودة </h1>
+     <h4> يرجى التاكد من المسار الصحيح او العودة للصفحة الرئيسية </h4>
+
+</div>
+
+
+@include('Front.include.footer')
