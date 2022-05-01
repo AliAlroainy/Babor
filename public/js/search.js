af46ea6aea7ec40542b7223058887433681b3aa7
ReemAlshathly <reem250139@gmail.com>
2022-05-01 07:56:26 +0300

    add function filter

--- public/js/search.js
+++ public/js/search.js
@@ -93,3 +93,43 @@ function filter_function1(){
 
 
 //////////////// filtering
+$('.filter2').change(function(){
+
+  filter_function1();
+  
+  //calling filter function each select box value change
+  
+});
+
+$('.Auction_card .status_filed ').show(); //intially all rows will be shown
+
+function filter_function1(){
+  $('.Auction_card .status_filed ').hide(); //hide all rows
+  
+  var companyFlag = 0;
+  var companyValue = $('#filter-status').val();
+
+  //setting intial values and flags needed
+  
+ //traversing each row one by one
+  $('.status_filed').each(function() {  
+  
+    if(companyValue == 0){   //if no value then display row
+    companyFlag = 1;
+    }
+    else if(companyValue == $(this).find('td.status').data('status')){ 
+      companyFlag = 1;       //if value is same display row
+    }
+    else{
+      companyFlag = 0;
+    }
+    
+    
+
+   if(companyFlag ){
+     $(this).show();  //displaying row which satisfies all conditions
+   }
+
+});   
+}
+
