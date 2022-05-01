1684e96e980585bf4c7d6b9d5ad337ce38d36741
ReemAlshathly <reem250139@gmail.com>
2022-05-01 07:50:53 +0300

    create car brand filter

--- public/js/search.js
+++ public/js/search.js
@@ -39,3 +39,50 @@ function filter_function(){
 
 });   
 }
+
+
+
+/////////////////////////filter brand
+
+
+//////////////// filtering
+$('.filter1').change(function(){
+
+  filter_function();
+  
+  //calling filter function each select box value change
+  
+});
+
+$('.Auction_card .brand_filed ').show(); //intially all rows will be shown
+
+function filter_function(){
+  $('.Auction_card .brand_filed ').hide(); //hide all rows
+  
+  var companyFlag = 0;
+  var companyValue = $('#filter-brand').val();
+
+  //setting intial values and flags needed
+  
+ //traversing each row one by one
+  $('.brand_filed').each(function() {  
+  
+    if(companyValue == 0){   //if no value then display row
+    companyFlag = 1;
+    }
+    else if(companyValue == $(this).find('td.brand').data('brand')){ 
+      companyFlag = 1;       //if value is same display row
+    }
+    else{
+      companyFlag = 0;
+    }
+    
+    
+
+   if(companyFlag ){
+     $(this).show();  //displaying row which satisfies all conditions
+   }
+
+});   
+}
+
