92d7128b2c918a581cc9e6f16f732c5c2fc71b11
Jehad Almaliki <yourgitemail@example.com>
2022-05-03 20:54:47 +0300

    feat : add a database insert statement to the run method

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -15,5 +15,16 @@ class BrandSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker::create();
+        foreach (range(1,5) as $index) {
+          DB::table('brands')->insert([
+              'name' => $faker->word(),
+              'logo' => $faker->image('public/images/categories',640,480, null, false),
+              'is_active' => $faker->numberBetween(1,3),
+              'created_at'=> $faker->now(),
+              'updated_at' => $faker->now(),
+
+          ]);
+    }
     }
 }
