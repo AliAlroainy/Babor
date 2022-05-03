10256eb860bd82635a8f3bdd143115a45860ea7d
Jehad Almaliki <yourgitemail@example.com>
2022-05-03 21:28:29 +0300

    feat : insert in to Seriseseeder

--- database/seeders/SeriesSeeder.php
+++ database/seeders/SeriesSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class SeriesSeeder extends Seeder
 {
@@ -15,5 +17,14 @@ class SeriesSeeder extends Seeder
     public function run()
     {
         //
+        $faker = Faker::create();
+        foreach (range(1,5) as $index) {
+          DB::table('series')->insert([
+              'name' => $faker->word(),
+              'brand_id' => $faker->$faker->numberBetween(1,5),
+              'is_active' => $faker->numberBetween(0,1),
+
+          ]);
     }
+}
 }
