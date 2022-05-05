1dc9c68063427cc65daf569bb05ac30b9e8033e8
A-Abd <abrar.abdulwahed@gmail.com>
2022-05-05 18:50:43 +0300

    fix: favorite page routes

--- app/Http/Controllers/admin/AcutionController.php
+++ app/Http/Controllers/admin/AcutionController.php
@@ -12,10 +12,23 @@ class AcutionController extends Controller
     {
         $auctions = Auction::orderBy('id')->get();
         return view('Admin.auctions.auctions')->with('auctions', $auctions);
-
     }
 
     public function action(Request $request, $id)
+    {
+        $auction = Auction::find($id);
+        if(!$auction)
+            return abort('404');
+        if ($request->input('approve')){
+            $action = Auction::where('id', $id)->update(['status' => '2']);
+        }
+        elseif ($request->input('disapprove')) {
+            $action = Auction::where('id', $id)->update(['status' => '1']);
+        }
+        return redirect()->back();
+    }
+
+    public function show(Request $request, $id)
     {
         $auction = Auction::find($id);
         if(!$auction)
