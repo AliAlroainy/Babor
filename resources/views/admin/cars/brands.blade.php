eeef4c78b967260720a2e1b7dda983f95bf56836
ReemAlshathly <reem250139@gmail.com>
2022-05-06 15:56:46 +0300

    update bouttons color in brands

--- resources/views/admin/cars/brands.blade.php
+++ resources/views/admin/cars/brands.blade.php
@@ -102,7 +102,7 @@
                                                                 <button type="button" class="btn btn-outline-secondary"
                                                                     data-bs-dismiss="modal">إلغاء</button>
                                                                 <button type="submit"
-                                                                    class="btn btn-primary text-white">تعديل</button>
+                                                                    class="btn btn-warning text-white">تعديل</button>
                                                             </div>
                                                         </div>
                                                     </form>
@@ -172,7 +172,7 @@
 
             </div>
             <button type="button" data-bs-target="#addModal" data-bs-toggle="modal"
-                class="btn btn-primary btn-rounded btn-icon add">
+                class="btn btn-warning btn-rounded btn-icon add">
                 <i class="mdi mdi-plus text-white"></i>
             </button>
         </div>
@@ -211,7 +211,7 @@
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">إلغاء</button>
-                            <button type="submit" class="btn btn-primary text-white">إضافة</button>
+                            <button type="submit" class="btn btn-warning text-white">إضافة</button>
                         </div>
                     </div>
                 </form>
