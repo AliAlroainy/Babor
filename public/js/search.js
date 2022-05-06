fa2b0360f74d3f32cf81bc546504bdc96b23ff84
ReemAlshathly <reem250139@gmail.com>
2022-05-06 03:18:32 +0300

    create function to filter car name

--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,22 @@
 
 
+    var searchFilter = () => {
+      let selectedCar = document.getElementById("filterByCar").value;
+      console.log(selectedCar);
+      // const input = document.querySelector(".form-control");
+      const cards = document.getElementsByClassName("series");
+      console.log(cards[1])
+      // let textBox = input.value;
+      for (let i = 0; i < cards.length; i++) {
+          // let title = cards[i].querySelector(".card-body");
+          if ((cards[i].classList.contains(selectedCar) || selectedCar=="") ) {
+              cards[i].classList.remove("d-none");
+          } else {
+              cards[i].classList.add("d-none");
+          }
+      }
+    }
+
 
 /////////////////////////filter brand
 
