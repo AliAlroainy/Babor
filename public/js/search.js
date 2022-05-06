d4cec42b7d3158718ed442e90d169fb25cb1a250
ReemAlshathly <reem250139@gmail.com>
2022-05-06 03:20:30 +0300

    create function to filter brands

--- public/js/search.js
+++ public/js/search.js
@@ -21,47 +21,23 @@
 /////////////////////////filter brand
 
 
-//////////////// filtering
-$('.filter1').change(function(){
-
-  filter_function1();
-  
-  //calling filter function each select box value change
-  
-});
-
-$('.Auction_card .brand_filed ').show(); //intially all rows will be shown
-
-function filter_function1(){
-  $(' .brand_filed ').hide(); //hide all rows
-  
-  var companyFlag = 0;
-  var companyValue = $('#filter-brand').val();
-
-  //setting intial values and flags needed
-  
- //traversing each row one by one
-  $('.brand_filed').each(function() {  
-  
-    if(companyValue == 0){   //if no value then display row
-    companyFlag = 1;
-    }
-    else if(companyValue == $(this).find('div.brand').data('brand')){ 
-      companyFlag = 1;       //if value is same display row
-    }
-    else{
-      companyFlag = 0;
-    }
-    
-    
-
-   if(companyFlag ){
-     $(this).show();  //displaying row which satisfies all conditions
-   }
-
-});   
+      
+var searchFilterBrand = () => {
+  let selectedCar = document.getElementById("filterByBrand").value;
+  console.log(selectedCar);
+  // const input = document.querySelector(".form-control");
+  const cards = document.getElementsByClassName("brand");
+  console.log(cards[1])
+  // let textBox = input.value;
+  for (let i = 0; i < cards.length; i++) {
+      // let title = cards[i].querySelector(".card-body");
+      if ((cards[i].classList.contains(selectedCar) || selectedCar=="") ) {
+          cards[i].classList.remove("d-none");
+      } else {
+          cards[i].classList.add("d-none");
+      }
+  }
 }
-
 
 
 
