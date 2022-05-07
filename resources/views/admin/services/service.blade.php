d7d2553523c4a174ca428f5f554664c0766c2fd8
ReemAlshathly <reem250139@gmail.com>
2022-05-08 01:42:18 +0300

    updates edite button in service

--- resources/views/admin/services/service.blade.php
+++ resources/views/admin/services/service.blade.php
@@ -125,9 +125,7 @@
                                                 </td>
                                                 <td>
                                                     <a href="editService" style="width: fit-content; font-size: 25px ;color:#686868"
-                                                        class="fa-solid fa-edit pe-2
-                                                        
-                                                         btn-fw btn-rounded "
+                                                        class="fa-solid fa-edit pe-2 btn-fw btn-rounded "
                                                         data-bs-target="#editModal-{{ $service->id }}"
                                                         data-bs-toggle="modal">
                                                         
