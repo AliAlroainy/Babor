55cf5124dceaa06f58d7a0beffda37312c82f075
Jehad Almaliki <yourgitemail@example.com>
2022-05-08 20:48:33 +0300

    view image  on the page below the FilePond drop area.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -88,7 +88,23 @@ FilePond.registerPlugin(
     const pond = FilePond.create(inputElement, {
   imageResizeTargetWidth:256,
 
- 
+  // add onaddfile callback
+  onaddfile: (err, fileItem) => {
+    console.log(err, fileItem.getMetadata('resize'));
+  },
+
+  // add onpreparefile callback
+  onpreparefile: (fileItem, output) => {
+    // create a new image object
+    const img = new Image();
+
+    // set the image source to the output of the Image Transform plugin
+    img.src = URL.createObjectURL(output);
+
+    // add it to the DOM so we can see the result
+    document.body.appendChild(img);
+  }
+
 });
     </script>
 
