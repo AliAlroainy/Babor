b460028e7985057d58e5109f4b06243cebb58e9b
ReemAlshathly <reem250139@gmail.com>
2022-05-09 13:41:22 +0300

    add servicesgow function

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -33,17 +33,14 @@ class SiteController extends Controller
         return response()->view('Front.404', []);
     }
     public function ServicesShow(){
-        // available = not-expired + progress
         $services = service::where('is_active', '1')->get();
         return view('Front.services', ['services' => $services]);
     }
     public function categoriesShow(){
-        // available = not-expired + progress
         $categories = Category::where('is_active', '1')->get();
         return view('Front.include.produacts', ['Categories' => $categories]);
     }
     public function questionShow(){
-        // available = not-expired + progress
         $questions = question::where('is_active', '1')->get();
         return view('Front.FAQ', ['questions' => $questions]);
     }
