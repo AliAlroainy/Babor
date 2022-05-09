c85f8c74899b63a30b87c1dbd8ff5c1b71a54522
ReemAlshathly <reem250139@gmail.com>
2022-05-09 23:57:26 +0300

    update view question front

--- resources/views/Front/FAQ.blade.php
+++ resources/views/Front/FAQ.blade.php
@@ -16,11 +16,11 @@
   <div class="accordion accordion-flush w-100" id="accordionFlushExample">
   @foreach ($questions as $question)
   <div class="accordion-item">
-      <h2 class="accordion-header" id="flush-headingOne">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+      <h2 class="accordion-header" id="flush-heading{{ $question->idText}}">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse{{ $question->idText}}" aria-expanded="false" aria-controls="flush-collapse{{ $question->idText}}">
         {{ $question->question}}        </button>
       </h2>
-      <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div id="flush-collapse{{ $question->idText}}" class="accordion-collapse collapse" aria-labelledby="flush-heading{{ $question->idText}}" data-bs-parent="#accordionFlushExample">
         <div class="accordion-body">{{ $question->answer}} .</div>
       </div>
     </div>
