3ba4a393228181891cba1fbdeca2d70725cb9370
Jehad Almaliki <yourgitemail@example.com>
2022-05-09 17:27:20 +0300

    new: create User Role seeder

--- database/seeders/AuctionSeeder.php
+++ database/seeders/AuctionSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
+use Faker\Factory as Faker;
 class AuctionSeeder extends Seeder
 {
     /**
@@ -14,6 +16,21 @@ class AuctionSeeder extends Seeder
      */
     public function run()
     {
+        $unixTimestamp = '1461067200';
+        $faker = Faker::create();
+        foreach (range(1,20) as $index) {
+          DB::table('auctions')->insert([
+              'closeDate' => $faker->date('Y-m-d', $unixTimestamp),
+              'minInc' => $faker->numberBetween(30000,60170),
+              'startDate' => $faker->date('Y-m-d', $unixTimestamp),
+              'openingBid' =>   $faker->numberBetween(40000,68017),
+              'reservePrice' => $faker->numberBetween(40000,68017),
+              'auctioneer_id' => $faker->numberBetween(1,2),
+              'car_id' => $faker->numberBetween(1,5),
+              'created_at'=> $faker->date('Y-m-d', $unixTimestamp),
+              'updated_at' => $faker->date('Y-m-d', $unixTimestamp),
 
+          ]);
+  }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 class UserSeeder extends Seeder
 {
     /**
@@ -16,14 +18,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $admin = new User;
-        $admin->name        = "admin";  
-        $admin->email       = "admin@gmail.com";
-        $admin->password    = Hash::make('123');
-        $admin->is_active   = 1;
-        $admin->created_at  = now();
-        $admin->updated_at  = now();
-        $admin->save();
-        $admin->attachRole('super_admin');
-    }
+        // $admin = new User;
+        // $admin->name        = "admin";
+        // $admin->email       = "admin@gmail.com";
+        // $admin->password    = Hash::make('123');
+        // $admin->is_active   = 1;
+        // $admin->created_at  = now();
+        // $admin->updated_at  = now();
+        // $admin->save();
+        // $admin->attachRole('super_admin');
+
+
+
+        $unixTimestamp = '1461067200';
+        $faker = Faker::create();
+        foreach (range(1,20) as $index) {
+        DB::table('users')->insert([
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => Hash::make('123'),
+        'is_active' => '1',
+        'created_at'=> $faker->date('Y-m-d', $unixTimestamp),
+       'email_verified_at'=> $faker->date('Y-m-d', $unixTimestamp),
+        'updated_at' => $faker->date('Y-m-d', $unixTimestamp),
+        'remember_token' => Str::random(10),
+
+    ]);
+}}
 }
