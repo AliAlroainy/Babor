a1e860d1174e3b822bb1cce5885b5c8175b43cbd
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-09 12:12:26 +0300

    add team img1

--- resources/views/Front/include/header.blade.php
+++ resources/views/Front/include/header.blade.php
@@ -27,6 +27,9 @@
     <!-- StyleSheet -->
 
     <!-- Bootstrap -->
+
+    <!-- icons -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <!-- Magnific Popup -->
     <link rel="stylesheet" href="/css/magnific-popup.min.css">
     <!-- Font Awesome -->
