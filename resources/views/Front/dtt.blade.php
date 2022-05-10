8e7be29d21cb41f2310da32eb730aa865b50ff8a
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-10 11:08:49 +0300

    feat :add model to auction

--- resources/views/Front/dtt.blade.php
+++ resources/views/Front/dtt.blade.php
@@ -126,7 +126,7 @@
                                     </div>
                                
                                 </div>
-                                <button class="add-to-cart-btn"><i class="fa fa-shopping-cart"></i> دخول
+                                <button class="add-to-cart-btn" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa fa-shopping-cart"></i> دخول
                                     بالمزاد</button>
                             </div>
                         </form>
@@ -186,6 +186,24 @@
 
 
 
+            <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
             <!-- Start Product Area -->
             {{-- <div class="product-area section" id="offer">
         <div class="container">
