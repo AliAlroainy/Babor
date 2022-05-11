3106371c1cec01f486690f0bd2dbcdd772241291
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-11 12:18:24 +0300

    add steps

--- resources/views/Front/sell.blade.php
+++ resources/views/Front/sell.blade.php
@@ -38,16 +38,17 @@
                                 <div class="date-content">
                                     <div class="date-outer">
                                         <span class="date">
-                                                <span class="month">1 Years</span>
-                                        <span class="year">2015</span>
-                                        </span>
+                                                <span class="month"></span>
+                                                <span class="year">ثانياً</span>
+                                            </span>
                                     </div>
                                 </div>
-                                <div class="timeline-content">
-                                    <h5 class="title">Product Designer</h5>
+                                <div class="timeline-content" >
+                                    <h5 class="title">  قم بالتسجيل بالموقع</h5>
                                     <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
+                                        من خلال صفحة التسجيل بالموقع ستقوم بخطوات بسيطة 
                                     </p>
+                                    <img src="img/Reg.png" width="300" height="300" alt="first" >
                                 </div>
                             </div>
                             <!-- end experience section-->
@@ -58,16 +59,17 @@
                                 <div class="date-content">
                                     <div class="date-outer">
                                         <span class="date">
-                                                <span class="month">2 Years</span>
-                                        <span class="year">2016</span>
-                                        </span>
+                                                <span class="month"></span>
+                                                <span class="year">ثالثاً</span>
+                                            </span>
                                     </div>
                                 </div>
-                                <div class="timeline-content">
-                                    <h5 class="title">Web Designer</h5>
+                                <div class="timeline-content" >
+                                    <h5 class="title">  قم بالتسجيل بالموقع</h5>
                                     <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
+                                        من خلال صفحة التسجيل بالموقع ستقوم بخطوات بسيطة 
                                     </p>
+                                    <img src="img/Reg.png" width="300" height="300" alt="first" >
                                 </div>
                             </div>
                             <!-- end experience section-->
@@ -78,16 +80,17 @@
                                 <div class="date-content">
                                     <div class="date-outer">
                                         <span class="date">
-                                                <span class="month">2 Years</span>
-                                        <span class="year">2018</span>
+                                                <span class="month"></span>
+                                        <span class="year">رابعاً</span>
                                         </span>
                                     </div>
                                 </div>
-                                <div class="timeline-content">
-                                    <h5 class="title">Graphic Designer</h5>
+                                <div class="timeline-content" >
+                                    <h5 class="title">  قم بالتسجيل بالموقع</h5>
                                     <p class="description">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur ex sit amet massa scelerisque scelerisque. Aliquam erat volutpat. Aenean interdum finibus efficitur. Praesent dapibus dolor felis, eu ultrices elit molestie.
+                                        من خلال صفحة التسجيل بالموقع ستقوم بخطوات بسيطة 
                                     </p>
+                                    <img src="img/Reg.png" width="300" height="300" alt="first" >
                                 </div>
                             </div>
                             <!-- end experience section-->
