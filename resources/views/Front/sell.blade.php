8c593b1e3ceebba376cbb66455d67954df03620c
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-11 12:07:38 +0300

    add button desc

--- resources/views/Front/sell.blade.php
+++ resources/views/Front/sell.blade.php
@@ -97,21 +97,15 @@
 
 
     
-<section class="section free-version-banner ">
+<section class="section free-version-banner mb-0 ">
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-8 offset-md-2 col-xs-12">
                 <div class="section-title mb-60">
-                    <span class="text-white wow fadeInDown" data-wow-delay=".2s"
-                        style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInDown;">عرووض بابور
-                        الحصرية</span>
+        
                     <h2 class="text-white wow fadeInUp" data-wow-delay=".4s"
-                        style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">يمكنك الان
-                        الاستفادة من العروض<br> والدخول بمزادات بابور.</h2>
-                    <p class="text-white wow fadeInUp" data-wow-delay=".6s"
-                        style="visibility: visible; animation-delay: 0.6s; animation-name: fadeInUp;">يرجى التسجيل
-                        بالموقع اولا والاستفاده بميزات المستخدم,<br> ولوحة التحكم الخاصة بك حيث يمكنك ااضافة ومشاركة
-                        بالمزادات.</p>
+                        style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">سجل الدخول في الموقع  لاضافة مزاد </h2>
+                  
 
                     <div class="button">
                         <a href="/register" target="_blank" rel="nofollow" class="btn wow fadeInUp"
