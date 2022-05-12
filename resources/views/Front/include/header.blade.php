753d9192b57f7d52efa07c714e84abce6e20472b
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-12 14:06:17 +0300

    fix : sell button reg

--- resources/views/Front/include/header.blade.php
+++ resources/views/Front/include/header.blade.php
@@ -113,9 +113,9 @@
         ga('send', 'pageview');
     </script>
 {{-- success --}}
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"> --}}
 
 </head>
 
