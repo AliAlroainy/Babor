595f82d9c5a71879c58c5a77b7f41cf657a91f2a
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-13 23:02:40 +0300

    feat : add confirmbuy page

---
+++ resources/views/Front/addtions/confirmBuy.blade.php
@@ -0,0 +1,39 @@
+@include('Front.include.header')
+
+
+<div class="container" >
+
+    <div>
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets1.lottiefiles.com/packages/lf20_cyn8dgwy.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+    </div>
+
+    <div>
+        <h1> تاكيد عملية الشراء بين الطرفين </h1>
+    </div>
+
+    <div>
+        <h4>  في حال تم التاكيد على البيع من قبل الطرفين سيقوم الموقع بتحويل المبلغ الى البائع</h4>
+    </div>
+
+    <div>
+        <p> تعليمات هامة  </p>
+        <ul>
+            <li>
+                يخلي الموقع كامل مسؤليته بعد عملية التاكيد
+            </li>
+            <li>
+                يجب التاكد بشكل كامل من قبل المشتري على استلام السيارة
+            </li>
+            <li>
+                في حال كان هناك عدم اتفاك او تراجع بالبيع يرجى  ضغط زر التراجع مع ذكر السبب
+            </li>
+            <li>
+                في حال تم التراجع على البيع من قبل احد الطرفين بدون مبرر سيتم خصم عقوبة
+            </li>
+        </ul>
+    </div>
+
+</div>
+
+@include('Front.include.footer')
