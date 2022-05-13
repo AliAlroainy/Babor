5e75088e4f8d2d64c180139fd29d61d9a8ba89a5
Jehad Almaliki <yourgitemail@example.com>
2022-05-13 21:04:38 +0300

    message table add

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,4 @@
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -6,25 +7,13 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+window.Vue = require('vue');
 
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import 'vuetify/dist/vuetify.min.css'
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -32,6 +21,13 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// const VueUploadComponent = require('vue-upload-component')
+// Vue.component('file-upload', VueUploadComponent)
+
+Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('Chat', require('./components/Chat.vue'));
+// Vue.component('PrivateChat', require('./components/PrivateChat.vue'));
+
 const app = new Vue({
-    el: '#app',
+    el: '#app'
 });
