9ebd5639fea046761f7ffe8ab0aa53c7cb38d843
ReemAlshathly <reem250139@gmail.com>
2022-05-14 21:50:28 +0300

    create auctions file in cntroller liveware for control the babor auctions

--- app/Http/Livewire/Babor.php
+++ app/Http/Livewire/Babor.php
@@ -24,5 +24,14 @@ class Babor extends Component
     // }
     public function render()
     {
-        
+        $last_babors = Auction::with(['car' => function ($q){
+            return $q->where('category_id', 4)->get();
+        }])->where('status', '2')->orderBy('id', 'desc')->paginate($this->pe_babor);
+        return view('livewire.babor')->with([
+           
+           
+            'last_babors'=> $last_babors,
+           
+        ]);
+    }
 }
