5d4d5c6ad336b36f11ac2f934259a1dba11b92cf
A-Abd <abrar.abdulwahed@gmail.com>
2022-05-14 16:14:32 +0300

    fix: change controller name of completing auction

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use App\Http\Controllers\Admin\ServicesController;
 use App\Http\Controllers\Admin\CategoriesController;
 use App\Http\Controllers\user\UserAuctionController;
 use App\Http\Controllers\Authentication\authcontroller;
-use App\Http\Controllers\admin\CompletingAuctionController;
+use App\Http\Controllers\admin\PaymentController;
 use App\Http\Controllers\Admin\CarCharacteristicsController;
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 use App\Http\Controllers\Authentication\ResetPasswordController;
@@ -135,7 +135,7 @@ Route::group(['middleware'=>'auth'],function(){
             Route::post('/auctions/in-progress/action/{id}', [UserAuctionController::class, 'action'])->name('user.progress.action.auction');
             Route::get('/bids', [BidController::class, 'index'])->name('user.show.bids');
             Route::post('/bid/{id}', [BidController::class, 'create'])->name('user.place.bid');
-            Route::post('/auction/{id}/buy', [CompletingAuctionController::class, 'buy'])->name('user.buy.auction');
+            Route::post('/auction/{id}/buy', [PaymentController::class, 'buy'])->name('user.buy.auction');
         });
         // Route::get('/change-password', [AuthController::class, 'changePasswordUser'])->name('change-password-user');
         Route::post('/change-password', [AuthController::class, 'updatePassword'])->name('update-password-user');
