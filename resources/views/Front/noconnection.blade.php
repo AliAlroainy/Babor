2250e67510d25916cb14a1469c3eb07cf8d51fcc
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-16 12:48:43 +0300

    feat : set no connaction page

--- resources/views/Front/noconnection.blade.php
+++ resources/views/Front/noconnection.blade.php
@@ -3,12 +3,15 @@
 
 <div class="container d-flex flex-column align-items-center justify-content-center w-100 h-100 mb-5" dir="rtl">
 
-    <img src="/svg/404.svg" width="500" alt="404" />
-    <h2> صفحة غير موجودة</h2>
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets2.lottiefiles.com/packages/lf20_6s2xGI.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+    <h2> لايوجد اتصال بالانترنت</h2>
 
-    <h4 class="text-muted mb-3"> يرجى التاكد من المسار الصحيح او العودة للصفحة الرئيسية </h4>
+    <h4 class="text-muted mb-3"> يرجى التاكد من اتصالك  بشبكة الانترنت </h4>
 
-    <a href="/" class="btn btn-warning"> الرئيسية</a>
+    <a href="/" class="btn btn-warning">
+        <i class="bi bi-arrow-clockwise"></i>
+        تحديث</a>
 
 </div>
 
