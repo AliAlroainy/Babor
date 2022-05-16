c476f7d824193cec71a6326b0975f10eeeeccd28
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-16 13:26:21 +0300

    feat : set 403 page

---
+++ resources/views/Front/403.blade.php
@@ -0,0 +1,17 @@
+@include('Front.include.header')
+
+
+<div class="container d-flex flex-column align-items-center justify-content-center w-100 h-100 mb-5" dir="rtl">
+
+    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+    <lottie-player src="https://assets8.lottiefiles.com/packages/lf20_4yztayzg.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player>
+    <h2> صفخة غير مسموح بها</h2>
+
+    <h4 class="text-muted mb-3"> غير مصرح لك الدخول الى هذا المسار </h4>
+
+    <a href="/" class="btn btn-warning"> العودة الى الصفحة السابقة</a>
+
+</div>
+
+
+@include('Front.include.footer')
