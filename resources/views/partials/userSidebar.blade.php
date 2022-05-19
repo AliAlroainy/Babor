78926b13d4652bd32441d20982db30579353f21e
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-19 12:43:08 +0300

    fix :side layouts

--- resources/views/partials/userSidebar.blade.php
+++ resources/views/partials/userSidebar.blade.php
@@ -4,15 +4,17 @@
         <ul class="nav " style="padding-inline: 0">
             <li class="nav-item">
                 <a class="nav-link" href="{{ route('user.profile') }}">
-                    <i class="fa-solid fa-user menu-icon ms-3 "></i>
-                    <span class="menu-title fw-bold " style="font-family: Tajawal">المعلومات الشخصية</span>
+                    <i class="fa-solid fa-user menu-icon me-3 ms-3 "></i>
+                    <span class="menu-title fw-bold " style="font-family: Tajawal">
+                        
+                        المعلومات الشخصية</span>
                 </a>
             </li>
 
             <li class="nav-item">
                 <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false"
                     aria-controls="ui-basic">
-                    <i class="mdi mdi-car menu-icon ms-3 "></i>
+                    <i class="mdi mdi-car menu-icon me-3 ms-3 "></i>
                     <span class="menu-title fw-bold" style="font-family: Tajawal">المزادات</span>
                     <i class="menu-arrow"></i>
                 </a>
@@ -22,6 +24,7 @@
                         <li class=" me-5 ">
                             <a class="nav-link" href="{{ route('user.add.auction') }}"
                                 style="font-family: Tajawal">
+                                <i class="bi bi-megaphone ms-2"></i>
                                 إضافة مزاد
                             </a>
                         </li>
@@ -30,10 +33,12 @@
                         <li class="me-5">
                             <a class="nav-link" data-bs-toggle="collapse" href="#show-auctions"
                                 aria-expanded="false" aria-controls="show-auctions">
-                                <span class="menu-title fw-bold" style="font-family: Tajawal; "> تفاصيل المزادات</span>
+                                <span class="menu-title fw-bold" style="font-family: Tajawal; ">
+                                    <i class="bi bi-cone-striped ms-2"></i>
+                                    تفاصيل المزادات</span>
                                 <i class="menu-arrow"></i>
                             </a>
-                            <div class="collapse" id="show-auctions">
+                            <div class="collapse " id="show-auctions"  >
                                 <ol class="list-unstyled nav flex-column sub-menu">
                                     <li class=" me-5" >
 
@@ -59,7 +64,7 @@
                                         <a class="nav-link" href="{{ route('user.show.pending.auction') }}"
                                             style="font-family: Tajawal">
                                             <i class="bi bi-hourglass-split ms-1"></i>
-                                             بانتظار موفقه المسؤول
+                                             بانتظار موافقة المسؤول
                                         </a>
                                     </li>
 
@@ -110,8 +115,8 @@
 
 
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('user.dashboard') }}">
-                    <i class=" mdi mdi-settings menu-icon ms-3"></i>
+                <a class="nav-link " href="{{ route('user.dashboard') }}">
+                    <i class=" mdi mdi-settings menu-icon  me-3 ms-3"></i>
                     <span class="menu-title fw-bold " style="font-family: Tajawal">إعدادات الحساب</span>
                 </a>
             </li>
