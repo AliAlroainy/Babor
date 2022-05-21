56adf49c999a0c3f4aada477c0cf4dd8a394da67
Jehad Almaliki <yourgitemail@example.com>
2022-05-21 10:04:49 +0300

    feat: add code to cypress

--- cypress/integration/input-babor-test.js
+++ cypress/integration/input-babor-test.js
@@ -6,6 +6,6 @@
 // https://on.cypress.io/writing-first-test
 describe('The Home Page', () => {
     it('successfully loads', () => {
-      cy.visit('http://localhost:8000') // change URL to match your dev URL
+      cy.visit('/') // change URL to match your dev URL
     })
   })
