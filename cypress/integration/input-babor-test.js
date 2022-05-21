b425c276bd7a5ee03f748c6b18dca25158afa8de
Jehad Almaliki <yourgitemail@example.com>
2022-05-21 06:38:12 +0300

    feat : add url to visit it in cy

--- cypress/integration/input-babor-test.js
+++ cypress/integration/input-babor-test.js
@@ -4,3 +4,8 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+describe('The Home Page', () => {
+    it('successfully loads', () => {
+      cy.visit('http://localhost:8000') // change URL to match your dev URL
+    })
+  })
