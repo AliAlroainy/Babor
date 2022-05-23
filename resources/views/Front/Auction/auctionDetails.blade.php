c21b5fd638bccf00dab1e8c2ce859132a424a40c
A-Abd <abrar.abdulwahed@gmail.com>
2022-05-23 21:08:56 +0300

    fix: error messages that appear during create auction

--- resources/views/Front/Auction/auctionDetails.blade.php
+++ resources/views/Front/Auction/auctionDetails.blade.php
@@ -1,12 +1,11 @@
-
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-  
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
 @extends('partials.usermaster')
 @section('body')
-<div class="main-panel">
+    <div class="main-panel">
         <div class="content-wrapper" style="position: relative">
             <div class="row">
                 <div class="col-lg-12 grid-margin stretch-card">
@@ -237,12 +236,12 @@
                                                     class="col-lg-12 "
                                                     src="/images/cars/{{ $auction->car->thumbnail }}" alt="image" />
                                                 <div class="">
-                                                   
+
 
                                                     <div id="demo" class="carousel slide" data-ride="carousel">
-                                                    <br><br>
+                                                        <br><br>
                                                         <h6 style="direction:ltr;margin-left:30%">عرض المزيد من الصور</h6>
-<br><br>
+                                                        <br><br>
                                                         <!-- Indicators -->
                                                         <ul class="carousel-indicators">
                                                             <li data-target="#demo" data-slide-to="0"
@@ -252,11 +251,11 @@
                                                         </ul>
 
                                                         <!-- The slideshow -->
-                                                        <div class="carousel-inner"  style="width:400px;">
+                                                        <div class="carousel-inner" style="width:400px;">
                                                             <div class="carousel-item active">
                                                                 <img src="/images/cars/{{ $auction->car->thumbnail }}"
-                                                                    style="width:100%;height:50%" alt=""
-                                                                    width="1100" height="500">
+                                                                    style="width:100%;height:50%" alt="" width="1100"
+                                                                    height="500">
                                                             </div>
                                                             @php
                                                                 $images = json_decode($auction->car->car_images, true);
@@ -264,8 +263,7 @@
                                                             @foreach ($images as $img)
                                                                 <div class="carousel-item">
                                                                     <img src="/images/cars/car_images/{{ $img }}"
-                                                                        alt="" width="1100" height="500"
-                                                                        style="width:100%">
+                                                                        alt="" width="1100" height="500" style="width:100%">
                                                                 </div>
                                                             @endforeach
 
