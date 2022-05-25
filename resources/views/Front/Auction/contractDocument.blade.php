fad123e84c84fbc296edb2d9abcb89707dbff444
Ali AL-Roainy <aliabduh.2020@gmail.com>
2022-05-25 21:26:46 +0300

    add : back icon

--- resources/views/Front/Auction/contractDocument.blade.php
+++ resources/views/Front/Auction/contractDocument.blade.php
@@ -7,6 +7,13 @@
         {{-- <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
         <lottie-player src="https://assets2.lottiefiles.com/packages/lf20_1dlnyjbb.json" background="transparent"
             speed="1" style="width: 300px; height: 300px;" loop autoplay></lottie-player> --}}
+
+            {{-- back  --}}
+{{--             
+            <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+            <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_wgupl2ia.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop  autoplay></lottie-player> --}}
+
+
     </div>
     <div class="text-center">
         <div class="">
