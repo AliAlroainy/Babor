b9f18d10e1b901177499a33ee26dc12c46ec7cd4
Jehad Almaliki <yourgitemail@example.com>
2022-10-06 09:45:11 +0300

    feat : edit on profile page

--- resources/views/user/email/forgetPassword.blade.php
+++ resources/views/user/email/forgetPassword.blade.php
@@ -5,7 +5,7 @@
         <div class="col-sm-6 border m-auto mt-5">
             <div class="text-center">
                 <h3><i class="fa fa-lock fa-2x my-3"></i></h3>
-                <h2 class="text-center">هل نسيت كلمة المرور ؟</h2>
+                <h2 class="text-center">هل نسيت كلمة المرور? ؟</h2>
                 <div class="panel-body my-5">
                     <form method="POST" action="/forget-password">
                       @csrf
